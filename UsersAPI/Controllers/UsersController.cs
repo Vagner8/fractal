917125450652cec12b852a3252a08fcfd5764c56
Dima_Vagner <myevropa1@gmail.com>
2024-01-11 11:50:07 +0200

    edit gitignore filr

---
+++ UsersAPI/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UsersAPI.Data;
+using UsersAPI.Models;
+
+namespace UsersAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController(AppDbContext db) : ControllerBase
+    {
+        private readonly AppDbContext _db = db;
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var response = new VegaShareNuget.Response<IEnumerable<User>>(await _db.Users.ToListAsync(), "");
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
--- UsersAPI/Program.cs
+++ UsersAPI/Program.cs
@@ -2,10 +2,18 @@ using Microsoft.EntityFrameworkCore;
 using UsersAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddCors(option =>
+{
+    option.AddPolicy(name: MyAllowSpecificOrigins, policy =>
+    {
+        policy.WithOrigins("http://localhost:4200");
+    });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,6 +30,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseHttpsRedirection();
 
